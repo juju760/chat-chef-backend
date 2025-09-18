07d01bdc72713df955441c3445857ca46bc1f852
juju760 <juju@nhis.or.kr>
2025-09-18 13:34:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("hello nodejs");
+/*
+package.json
+"dependencies": {
+    "cors": "^2.8.5", //다른 출처의 선택한 리소스에 접근할 수 있는 권한을 부여하도록 브라우저에 알려주는 메커니즘
+    "dotenv": "^17.2.2", //개인정보를 손쉽게 처리할수있는 
+    "express": "^5.1.0",
+    "openai": "^5.21.0" //이 라이브러리를 사용해야 api 연결 가능
+  }
+
+쌤 노션: https://charmed-summer-94a.notion.site/AI-13ebc95bcb948089a981d4119268b71b#13ebc95bcb9480098b81e1d1b7f58cfd
+깃헙: https://github.com/juju760/chat-chef-frontend
+*/
